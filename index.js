4c9a85d33705eb0cdca394475bf47dbb7db60b47
Rosa Tu <rosalina.tu@gmail.com>
2018-06-01 19:55:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+function arrays()
+{
+  }
+
+function destructivelyAppendKitten(name) 
+{
+return kittens.push(name)
+}
+function destructivelyPrependKitten(name)
+{
+return kittens.unshift(name)  
+}
+function destructivelyRemoveLastKitten() 
+{
+  return kittens.pop(name)  
+}
+function destructivelyRemoveFirstKitten()
+{
+  return kittens.shift(name) 
+}
+function appendKitten(name)
+{
+  return [...kittens, name]
+}
+function prependKitten(name)
+{
+  return [name, ...kittens]
+}
+function removeLastKitten()
+{
+  
+}
+function removeFirstKitten()
+{
+  return kittens.slice(1)
+}
